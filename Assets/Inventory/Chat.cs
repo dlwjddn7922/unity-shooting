3b9f7aaf6d9a69d96990ed42a241c90237d72c6d
KMH0309 <105356862+KMH0309@users.noreply.github.com>
2023-07-21 19:53:52 +0900

    채팅

--- Assets/Inventory/Chat.cs
+++ Assets/Inventory/Chat.cs
@@ -27,16 +27,18 @@ public class Chat : MonoBehaviour
     [SerializeField] Text chat;
     [SerializeField] Transform parent;
     [SerializeField] TMPro.TMP_InputField myChat;
-
+    TMPro.TMP_InputField inputField;
     string[] charType = { "Jin", "Ari", "Sona", "Teemo" };
-    string[] nickType = { "Player1", "Player2", "Player3", "Player4" };
+    string[] monsterType = { "바론 처치", "드레곤 처치", "장로드레곤 처치", "전령 처치" };
     string[] chatType = { "바론 ㄱㄱ", "탑 ㄱㄱ", "미드 ㄱㄱ", "봇 ㄱㄱ", };
+    string[] charType1 = { "Player1 (Jin)", "Player2 (Ari)", "Player3 (Sona) ", "Player4 (Teemo) ", };
 
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("Chatting", 1f, 3f);
+        InvokeRepeating("MonsterChatting", 3f, 6f);
     }
 
     // Update is called once per frame
@@ -46,21 +48,31 @@ public class Chat : MonoBehaviour
     }
     public void Chatting()
     {
-        string charkey = charType[Random.Range(0, charType.Length)];
-        string nickkey = nickType[Random.Range(0, nickType.Length)];
         string chatkey = chatType[Random.Range(0, chatType.Length)];
-        chat.text = $"<b><color=blue>({charkey}){nickkey}</color></b> :{chatkey}";
+        string char1key = charType1[Random.Range(0, charType1.Length)];
+        chat.text = $"<b><color=blue>{char1key}</color></b> :{chatkey}";
         Instantiate(chat, parent);
     }
-    public void CreateData()
+    public void MonsterChatting()
     {
-        string charkey = charType[Random.Range(0, charType.Length)];
-        string nickkey = nickType[Random.Range(0,nickType.Length)];
+        string char1key = charType1[Random.Range(0, charType1.Length)];
+        string monkey = monsterType[Random.Range(0, monsterType.Length)];
+        chat.text = $"<b><color=blue>{char1key}님</color></b><b><color=red> {monkey}</color></b>";
+        Instantiate(chat, parent);
     }
     public void OnChatSend()
     {
-        chat.text = $"<b><color=blue>(Ash)lee</color></b>:  {myChat.text}"; 
-        Instantiate(chat, parent);
-        chat.text = "";
+        if(myChat.text != "")
+        {
+            chat.text = $"<b><color=blue>(Ash)lee</color></b>:  {myChat.text}";
+            Instantiate(chat, parent);
+            chat.text = "";
+            myChat.text = "";
+        }else
+        {
+            Debug.Log("채팅을 입력하세요.");
+        }
+        
+        
     }
 }
